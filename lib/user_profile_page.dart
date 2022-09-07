e28d62d0dc8291cafad5908491d47f4c70ad8891
ponganan <joeyubu@gmail.com>
2022-09-07 23:49:14 +0700

    change User Profile Page

--- lib/user_profile_page.dart
+++ lib/user_profile_page.dart
@@ -11,28 +11,86 @@ class UserProfilePage extends StatefulWidget {
 class _UserProfilePage extends State<UserProfilePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  final _controllerName = TextEditingController();
+  final _controllerTel = TextEditingController();
+  final _controllerCity = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Profile Page'),
+        title: const Text(
+          'User Profile Page',
+        ),
+        actions: [
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: Icon(Icons.logout),
+              )),
+        ],
       ),
-      body: Center(
-        child: Column(children: [
+      body: ListView(
+        padding: EdgeInsets.all(15),
+        children: <Widget>[
           const SizedBox(height: 28),
           Text(
-            'Signed In as : ' + user.email! + ' and UID : ' + user.uid,
-            style: const TextStyle(fontSize: 20),
+            'ตั้งค่าบัญชี',
+            style: const TextStyle(fontSize: 22),
+            textAlign: TextAlign.center,
           ),
           const SizedBox(height: 15),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            color: Colors.green[200],
-            child: const Text('Sign Out'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              controller: _controllerName,
+              decoration: decorationTF('ชื่อ'),
+            ),
           ),
-        ]),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              controller: _controllerTel,
+              decoration: decorationTF('เบอร์โทร'),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              controller: _controllerCity,
+              decoration: decorationTF('จังหวัด'),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: MaterialButton(
+              onPressed: () {},
+              color: Colors.green[200],
+              child: const Text(
+                'Save',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  InputDecoration decorationTF(String label) => InputDecoration(
+        labelText: label,
+        contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        //use OutlineInputBorder to Border all Textfield
+
+        border: const OutlineInputBorder(),
+      );
 }
--- lib/verify_email_page.dart
+++ lib/verify_email_page.dart
@@ -78,31 +78,31 @@ class _VerifyEmailPageState extends State<VerifyEmailPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
+                const Text(
                   'A verification email has been sent to your email.',
                   style: TextStyle(fontSize: 20),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 25),
+                const SizedBox(height: 25),
                 ElevatedButton.icon(
                   style: ElevatedButton.styleFrom(
-                    minimumSize: Size.fromHeight(50),
+                    minimumSize: const Size.fromHeight(50),
                   ),
-                  icon: Icon(
+                  icon: const Icon(
                     Icons.email,
                     size: 35,
                   ),
-                  label: Text(
+                  label: const Text(
                     'Resent Email',
                     style: TextStyle(fontSize: 20),
                   ),
                   onPressed: canResendEmail ? sendVerificationEmail : null,
                 ),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 TextButton(
                   style: ElevatedButton.styleFrom(
-                      minimumSize: Size.fromHeight(50)),
-                  child: Text(
+                      minimumSize: const Size.fromHeight(50)),
+                  child: const Text(
                     'Cancel',
                     style: TextStyle(fontSize: 20),
                   ),
